ec4bd6d35732e60bc09ea3737af1e9bd7e1e3694
Mikkel Vilstrup <Mikkel@vilstrup.dk>
2015-03-23 18:25:06 +0100

    Updated requirements
    I updated requirements.txt to enable the installation of the necessary packages
    through pip

--- setup_webcrawling.sh
+++ setup_webcrawling.sh
@@ -3,9 +3,8 @@ sudo apt-get upgrade -y
 sudo apt-get update -y
 
 sudo apt-get install python-dev python-numpy python-tk python-bs4 -y
-sudo apt-get install python-pip python-scipy rabbitmq-server -y
+sudo apt-get install python-pip python-scipy -y
 sudo easy_install -U distribute
 sudo apt-get install libfreetype6-dev libxft-dev -y
 sudo apt-get install python-matplotlib -y
-sudo pip install six scikit-learn  celery
 sudo pip install -U matplotlib
